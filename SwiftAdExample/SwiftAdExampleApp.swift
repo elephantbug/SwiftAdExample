3772cef1d3c73d9c94da39b7a6201ccd53e876f5
Dmitriano <winappdev@gmail.com>
2024-09-25 01:02:55 -0700

    Initial Commit

---
+++ SwiftAdExample/SwiftAdExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAdExampleApp.swift
+//  SwiftAdExample
+//
+//  Created by Administrator on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAdExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
